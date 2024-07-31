6f735fa218556354395d652e845680dfef60029b
DXatom <adolf5341@gmail.com>
2024-08-01 01:22:36 +0300

    реализован хелпер для подсветки текста по запросу поиска

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,12 @@
 module PostsHelper
   include Pagy::Frontend
+
+  def search_highlight(content)
+    query = params[:query]
+    if query
+      content.gsub query, '<span class="bg-warning">' + (query) + '</span>'
+    else
+      content
+    end
+  end
 end
